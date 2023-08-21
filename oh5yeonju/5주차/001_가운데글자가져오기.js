2a1550034f498d8f2bb2fd9a712dc64308e700b1
Oh5Yeonju <oh5yeonju@gmail.com>
2023-08-21 10:35:59 +0900

    ✨feat: week_05-가운데 글자 가져오기

---
+++ oh5yeonju/5주차/001_가운데글자가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = '';
+    if (s.length % 2) {
+        answer = s[Math.floor(s.length/2)];
+    } else {
+        answer = s[s.length / 2 - 1] + s[s.length / 2];
+    }
+    return answer;
+}
