c00e21896f6bdd0144d534dcc1e7ad546e8392ed
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-29 12:26:55 +0900

    add: 삭제된 수박수.js

--- oh5yeonju/5주차/002_수박수박수박수박수박수?.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    const watermelon =['수', '박'];
-
-    let result = '';
-    for (let i = 0; i < n; i++) {
-        result += watermelon[i % 2];
-    }
-    return result;
-}
