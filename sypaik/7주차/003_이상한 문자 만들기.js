66e16a666c2e7bef0fe0fa43e40b35f8c17c9591
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-05 23:30:39 +0900

    ✨ feat: week_07-이상한 문자 만들기

--- sypaik/7주차/003_이상한 문자 만들기.js
+++ sypaik/7주차/003_이상한 문자 만들기.js
@@ -16,16 +16,7 @@ s	return
 */
 
 function solution(s) {
-    return s
-        .split(' ')
-        .map((i) =>
-            i
-                .split('')
-                .map((value, index) =>
-                    index % 2 === 0
-                        ? value.toUpperCase()
-                        : value.toLowerCase()
-                ).join('')
-        )
-        .join(' ');
-}
+  return s.split(' ').map((i) => i.split('').map((value, index) =>
+  index % 2 === 0 ? value.toUpperCase() : value.toLowerCase()).join('')
+  ).join(' ')
+};
