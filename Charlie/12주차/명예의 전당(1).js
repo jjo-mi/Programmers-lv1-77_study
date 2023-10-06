e6233fa17fd9f051b0a3cf707b3412160ce83698
d-charlie-kim <d.charlie.kim@gmail.com>
2023-10-07 01:02:22 +0900

    Solved: 12주차 3문제 해결

---
+++ Charlie/12주차/명예의 전당(1).js
@@ -0,0 +1,22 @@
+// 명예의 전당 (1)
+// https://school.programmers.co.kr/learn/courses/30/lessons/138477
+
+function solution(k, score) {
+  const stage = [];
+  const answer = [];
+
+  for (const num of score) {
+    if (stage.length < k) {
+      stage.push(num);
+    } else if (stage[0] < num) {
+      stage.shift();
+      stage.push(num);
+    }
+    stage.sort((a, b) => a - b);
+    answer.push(stage[0]);
+    console.log(stage);
+  }
+  return answer;
+}
+
+// [작은수, 큰수, ..., k] 순서로 저장하여 0번째 인덱스로만 비교
